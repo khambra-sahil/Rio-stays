3899a48fbc8cca24e94ea8409bedfa8e69a80249
Sahil Khambra <sahil.khambra@trigyn.com>
2020-06-13 13:15:03 +0530

    Maintain user history and config push

---
+++ web/modules/custom/guest_app_custom/src/EventSubscriber/UserCreateAlterEventSubscriber.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\guest_app_custom\EventSubscriber;
+
+use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+
+/**
+ * The class is used to implement event for form alter.
+ *
+ * @package Drupal\trigyn_wiki_form_alter\EventSubscriber
+ */
+class UserCreateAlterEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::ENTITY_PRE_SAVE => 'userCreateAlter',
+    ];
+  }
+
+  /**
+   * Implements hook_entity_create().
+   *
+   * @param array $event
+   *   Event array.
+   */
+  public function userCreateAlter($event) {
+    $entity = $event->getEntity();
+    if($entity->getEntityTypeId() == 'user'){
+      $uid = $entity->id();
+      $old_checkout_date_value = 0;
+      $current_date = \Drupal::time()->getCurrentTime();
+      if(!empty($entity->get('field_check_in_checkout_date'))) {
+        $new_check_in_out_date = $entity->get('field_check_in_checkout_date')->getValue();
+        $new_checkin_date_value = $new_check_in_out_date[0]['value'];
+        $check_in_date_read_able = date('d-M-Y',$new_checkin_date_value );
+        $new_checkout_date_value = $new_check_in_out_date[0]['end_value'];
+        $check_out_date_read_able = date('d-M-Y',$new_checkout_date_value );
+      }
+      if($entity->original != NULL) {
+        $old_checkout_date = $entity->original->get('field_check_in_checkout_date')->getValue();
+        $old_checkout_date_value = $old_checkout_date[0]['end_value'];
+      }
+      if(!empty($entity->get('field_hotel')->getValue())) {
+        $hotel_id = $entity->get('field_hotel')->getValue();
+        $hotel_load = \Drupal\node\Entity\Node::load($hotel_id[0]['target_id']);
+        $hotel_title = $hotel_load->getTitle();
+      }
+      if(!empty($entity->get('field_from_city'))){
+        $from_city = $entity->get('field_from_city')->getValue();
+      }
+      if(!empty($entity->get('field_from_state'))){
+        $from_state = $entity->get('field_from_state')->getValue();
+      }
+      if(!empty($entity->get('field_plan_type'))){
+        $plan_type = $entity->get('field_plan_type')->getValue();
+      }
+      if(!empty($entity->get('field_room_type'))){
+        $room_type = $entity->get('field_room_type')->getValue();
+      }
+      $first_name = $entity->get('field_first_name')->getValue();
+      $last_name = $entity->get('field_last_name')->getValue();
+      $message = $first_name[0]['value'].' '.$last_name[0]['value']. ' stay in '. $hotel_title .' from '.$check_in_date_read_able. ' to '.$check_out_date_read_able;   ;
+      if($new_checkout_date_value > $old_checkout_date_value && !empty($hotel_id)) {
+        $request = Node::create(['type' => 'user_history']);
+        $request->set('title', $message);
+        $request->set('field_from_city', $from_city[0]['value']);
+        $request->set('field_from_state', $from_state[0]['value']);
+        $request->set('field_plan_type', $plan_type[0]['target_id']);
+        $request->set('field_room_type', $room_type[0]['target_id']);
+        $request->set('field_user_history_hotel', $hotel_id[0]['target_id']);
+        $request->set('field_user_id', $uid);
+        //$request->set('field_check_in_check_out_details', array($new_checkin_date_value,$new_checkout_date_value));
+        //$request->set('field_check_in_check_out_details_end_value', $new_checkout_date_value);
+        $request->enforceIsNew();
+        $request->save();
+      }
+    }
+
+  }
+}
---
+++ web/modules/custom/guest_app_custom/src/EventSubscriber/UserFormAlterEventSubscriber.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\guest_app_custom\EventSubscriber;
+
+use Drupal\Core\Url;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\hook_event_dispatcher\HookEventDispatcherInterface;
+
+/**
+ * The class is used to implement event for form alter.
+ *
+ * @package Drupal\etisc_form_alter\EventSubscriber
+ */
+class UserFormAlterEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      HookEventDispatcherInterface::FORM_ALTER => 'loginFormAlter',
+    ];
+  }
+
+  /**
+   * Implements hook_form_alter().
+   *
+   * @param array $event
+   *   Event array.
+   */
+  public function loginFormAlter($event)
+  {
+    $current_form_id = $event->getFormId();
+    $form = $event->getForm();
+    if ($current_form_id == 'user_form') {
+      $allowed_fields = array('field_check_in_checkout_date','field_hotel','field_room_number',
+        'field_plan_type','field_room_type','field_from_city','field_from_state');
+      $current_user = \Drupal::currentUser();
+      $roles = $current_user->getRoles();
+      if(in_array('front_office', $roles)) {
+        $form['account']['#access'] = FALSE;
+        $form['user_picture']['#access'] = FALSE;
+        $form['contact']['#access'] = FALSE;
+        $form['actions']['delete']['#access'] = FALSE;
+        //kint($form);exit();
+        foreach($form as $key => $value) {
+          if(strstr($key, 'field_') &&!in_array($key,$allowed_fields)) {
+            $form[$key]['#access'] = FALSE;
+          }
+        }
+      }
+    }
+    $event->setForm($form);
+  }
+}
